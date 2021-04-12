c96165faf62ebe7c78b5bde088cdaddb02d8d9dc
Claire T <claire.thornewill@parcellab.com>
2021-04-12 17:08:17 +0200

    Replacing data from API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 function displayTemperature(response) {
-console.log(response.data.main.temp);
+let temperatureElement = document.querySelector(".temperature");
+let cityElement = document.querySelector("h1");
+let nowElement = document.querySelector(".now");
+let countryElement = document.querySelector(".country");
+let feelsLikeElement = document.querySelector(".feelsLike")
+let humidityElement = document.querySelector (".humidity")
+let windElement = document.querySelector(".windSpeed")
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+nowElement.innerHTML = response.data.weather[0].description;
+countryElement.innerHTML = response.data.sys.country;
+feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+
+
+
+console.log(response);
+
 }
 
 
