fb4c4c38d9b0ab5f6f7c5e715bbb6f3bebc3d2dd
Claire T <claire.thornewill@parcellab.com>
2021-04-12 19:17:39 +0200

    Added Date and Time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let month = months[date.getMonth()];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+    return `${day} ${month}`
+}
+
+
 function displayTemperature(response) {
 let temperatureElement = document.querySelector(".temperature");
 let cityElement = document.querySelector("h1");
@@ -6,6 +16,7 @@ let countryElement = document.querySelector(".country");
 let feelsLikeElement = document.querySelector(".feelsLike")
 let humidityElement = document.querySelector (".humidity")
 let windElement = document.querySelector(".windSpeed")
+let dateElement = document.querySelector(".date");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 nowElement.innerHTML = response.data.weather[0].description;
@@ -13,10 +24,8 @@ countryElement.innerHTML = response.data.sys.country;
 feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
+dateElement.innerHTML = formatDate(response.data.dt * 1000)
 
-
-
-console.log(response);
 
 }
 
